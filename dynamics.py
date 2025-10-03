cd04de8fc47ba4fbf6f78440da47c68bbffbf992
Lukasz Machura <lukasz.machura@gmail.com>
2025-10-03 16:23:41 +0200

    start of repo

---
+++ dynamics.py
@@ -0,0 +1,135 @@
+import pickle
+import time
+import os
+import ionchannels as ic
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from scipy.stats import entropy as scipy_shannon
+from scipy.stats import mode
+import EntropyHub as EH
+# from scipy import signal
+# from PyEMD import EMD
+
+def my_shannon(x, base=None, bins=33): 
+    """ Computes Shanon entropy """
+    x = np.asarray(x)
+    hx, bx = np.histogram(x, bins=bins, density=False)
+    hx = np.asarray(hx, dtype=np.float64)
+    hx /= x.size
+    hx = hx[hx>0]
+    
+    if base == None:
+        base = np.e
+        
+    SE = -np.sum(hx * np.log(hx)) / np.log(base)
+    return SE
+
+
+if __name__ == '__main__':
+    # check if traj, sampen, plots directories exist, if not create them
+    if not os.path.exists('traj'):
+        os.makedirs('traj')
+    if not os.path.exists('sampen'):
+        os.makedirs('sampen')
+    if not os.path.exists('plots'):
+        os.makedirs('plots')
+
+    # flags
+    calc_trajectory = True
+    calc_sampen = True
+    plot_sampen = True
+
+    # model
+    icm = ic.IonChannelModel(states={'open': [1], 'open value': -1, 'close': [5], 'close value': 1})
+    icm.full_stats = True
+    print(icm)
+
+    # simulation parameters
+    h = 0.002
+    tend = 50
+    D = 0.01
+    # blist = [(0.1, 0.5), (0.1, 0.9), (0.1, 1), (0.1, 1.1), (0.1, 1.5), (0.1, 2), (0.1, 5), (0.1, 10)]
+    # blist = [(0.5, 0.1), (0.5, 0.9), (0.5, 1), (0.5, 1.1), (0.5, 1.5), (0.5, 2), (0.5, 5), (0.5, 10)]
+    # blist = [(0.9, 0.1), (0.9, 0.5), (0.9, 1), (0.9, 1.1), (0.9, 1.5), (0.9, 2), (0.9, 5), (0.9, 10)]
+    # blist = [(1, 0.1), (1, 0.5), (1, 0.9), (1, 1.1), (1, 1.5), (1, 2), (1, 5), (1, 10)]
+    # blist = [(1.1, 0.1), (1.1, 0.5), (1.1, 0.9), (1.1, 1), (1.1, 1.5), (1.1, 2), (1.1, 5), (1.1, 10)]
+    # blist = [(1.5, 0.1), (1.5, 0.5), (1.5, 0.9), (1.5, 1), (1.5, 1.1), (1.5, 2), (1.5, 5), (1.5, 10)]
+    # blist = [(2.0, 0.1), (2.0, 0.5), (2.0, 0.9), (2.0, 1), (2.0, 1.1), (2.0, 1.5), (2.0, 5), (2.0, 10)]
+    # blist = [(5.0, 0.1), (5.0, 0.5), (5.0, 0.9), (5.0, 1), (5.0, 1.1), (5.0, 1.5), (5.0, 2), (5.0, 10)]
+    blist = [(10.0, 0.1), (10.0, 0.5), (10.0, 0.9), (10.0, 1), (10.0, 1.1), (10.0, 1.5), (10.0, 2), (10.0, 5)]
+    # blist = [(0.1, 0.1), (0.5, 0.5), (0.9, 0.9), (1, 1), (1.1, 1.1), (1.5, 1.5), (2, 2), (5, 5), (10, 10)] 
+
+    bfreq = str(mode(np.array(blist).flatten()).mode).replace('.','')
+
+    if not calc_trajectory and not calc_sampen and not plot_sampen: 
+        print("Nothing to do. Exiting.")
+        exit(0)
+
+    if calc_trajectory:
+        print("Starting simulations...")
+        for b in blist:
+            bm = b[0]
+            bp = b[1]
+            print(f"Simulating for bplus = {bp}, bminus = {bm}")
+            x = icm.simulate(tend=tend, h=h, D=D, levy_stat=False, 
+                            force={
+                                'potential': 'asym_quadratic', 
+                                'params': {'bminus': bm, 'bplus': bp}
+                                }
+                            )
+
+            with open(f'traj/dynamics_simulation_bplus_{bp}_bminus_{bm}.pkl', 'wb') as f:
+                pickle.dump(x, f)
+                print(f"Simulation data saved to traj/dynamics_simulation_bplus_{bp}_bminus_{bm}.pkl")
+
+    if calc_sampen:
+        print("Calculating SampEn...", blist)
+        m = 10
+        for b in blist:
+            print(f"Processing bplus = {b[1]}, bminus = {b[0]}")
+            bm = b[0]
+            bp = b[1]
+            with open(f'traj/dynamics_simulation_bplus_{bp}_bminus_{bm}.pkl', 'rb') as f:
+                x = pickle.load(f)
+                print(f"Loaded simulation data from traj/dynamics_simulation_bplus_{bp}_bminus_{bm}.pkl")
+
+            print(f"Computing SampEn for bplus = {bp}, bminus = {bm}")
+            t1 = time.time()
+            sampen = EH.SampEn(x, m=m) #, r=0.2*np.std(x))
+            t2 = time.time()
+            print(f"SampEn computed in {t2 - t1:.4f} seconds")
+
+            with open(f'sampen/sampen_bplus_{bp}_bminus_{bm}.pkl', 'wb') as f:
+                pickle.dump(sampen, f)
+                print(f"SampEn data saved to sampen/sampen_bplus_{bp}_bminus_{bm}.pkl")
+
+    if plot_sampen:
+        print("Plotting SampEn results...")
+        m = 10
+        plt.figure(figsize=(10, 6))
+        for b in blist:
+            bm = b[0]
+            bp = b[1]
+            with open(f'sampen/sampen_bplus_{bp}_bminus_{bm}.pkl', 'rb') as f:
+                sampen = pickle.load(f)
+                print(f"Loaded SampEn data from sampen/sampen_bplus_{bp}_bminus_{bm}.pkl")
+                plt.semilogy(range(m+1), sampen[0], '-o', label=f'b+={bp}, b-={bm}', alpha=0.7)
+        plt.xlabel('Embedding Dimension m')
+        plt.ylabel('SampEn')
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(f'plots/sampen_plot_bm{bfreq}.png', dpi=300)
+        
+
+    # plt.figure(figsize=(10, 6))
+    # plt.plot(np.arange(0, len(x) * h, h), x, label='Trajectory', alpha=0.7)
+    # plt.title('Ion Channel Dynamics Simulation')
+    # plt.xlabel('Time')
+    # plt.ylabel('Position')
+    # plt.grid(True)
+    # plt.legend()
+    # plt.tight_layout()
+    # plt.savefig('dynamics_simulation.png', dpi=300)
+    # plt.show()
---
+++ ionchannels.py
@@ -0,0 +1,356 @@
+import numpy as np
+
+
+def my_levy_stable(alpha=1.0, beta=0.0, loc=0.0, scale=1.0):
+    mu, sigma = loc, scale
+    V = np.pi * np.random.random() - np.pi/2
+    W = -np.log(1 - np.random.random())
+    if alpha == 1:
+        dzeta = mu + 2 * sigma * ((np.pi/2 + beta*V) * np.tan(V) - 
+                beta * np.log((np.pi * W * np.cos(V)/2)/(np.pi/2 + beta*V))) / np.pi
+    else:
+        C = np.arctan(beta * np.tan(np.pi * alpha/2)) / alpha
+        D = sigma * (np.cos(np.arctan(beta * np.tan(np.pi * alpha/2))))**(-1/alpha)
+        dzeta = D * np.sin(alpha*(V + C))*(np.cos(V - alpha*(V + C))/W)**(1/alpha - 1) / np.cos(V)**(1/alpha)
+    return dzeta
+
+
+class IonChannelModel:
+    state = ['open', 'close']
+    
+    def __init__(self, states={}, full_stats=False):
+        self.full_stats = full_stats
+        self.statistics = []
+        self.states = states
+        
+    @property
+    def states(self):
+        return self.__states
+    
+    @states.setter
+    def states(self, s):
+        assert isinstance(s, dict), 'states should be a dictionary'
+        self.__states = s
+        
+        if 'open' not in self.states:
+            self.__states['open'] = [1]
+            
+        if 'open value' not in self.states:
+            self.__states['open value'] = 1
+        
+        if 'close' not in self.states:
+            self.__states['close'] = [1]
+            
+        if 'close value' not in self.states:
+            self.__states['close value'] = -1
+        
+        # all states
+        len_states = len(self.states['open']) + len(self.states['close'])
+        
+        if 'scheme' not in self.states:
+            # first open, then close, no loops
+            self.__states['scheme'] = [[0 if i==j else 1 for j in range(len_states)] for i in range(len_states)]
+            self.__states['scheme choice'] = 'all allowed'
+        else:
+            self.__states['scheme choice'] = 'user'
+            if self.states['scheme'] == 'random':
+                self.__states['scheme choice'] += ' random'
+                self.random_scheme()
+                
+            if (len(self.states['scheme']) != len_states 
+                or
+                len(self.states['scheme'][0]) != len_states):
+                raise ValueError('scheme dimentions error')
+        
+        if 'inital state' not in self.states:
+            self.__states['initial state'] = self.next_state()
+    
+    def generate_random_scheme(self):
+        N = len(self.states['open']) + len(self.states['close'])
+        M = np.random.randint(0, 2, (N, N))
+        for idx in range(N):
+            for jdx in range(N):
+                if idx == jdx:
+                    M[idx][jdx] = 0
+        return M
+    
+    def scheme_ok(self, M):
+        '''
+        at least one 1 should be in each column AND row;
+        mind the zeros on diagonal;
+        '''
+        M = np.asarray(M)
+        nrows, ncols = M.shape
+        for irow in range(nrows):
+            if not any(M[irow, :]):
+                return False
+        for icol in range(ncols):
+            if not any(M[:, icol]):
+                return False
+        return True
+    
+    def random_scheme(self):
+        '''returns rate matrix NxN'''
+        N = len(self.states['open']) + len(self.states['close'])
+        M = np.zeros((N, N))
+        
+        while not self.scheme_ok(M):
+            M = self.generate_random_scheme()
+            
+        self.__states['scheme'] = M
+    
+    @property
+    def full_stats(self):
+        return self.__full_stats
+    
+    @full_stats.setter
+    def full_stats(self, val):
+        assert isinstance(val, bool), 'full_stats: True/False'
+        self.__full_stats = val
+    
+    @property
+    def statistics(self):
+        return self.__statistics
+    
+    @statistics.setter
+    def statistics(self, val):
+        self.__statistics = val
+
+    def update_stats(self, val):
+        self.statistics.append(val)
+    
+    @staticmethod
+    def draw_dwell_time(tau=1.0):
+        """
+        Generate a random dwell time sampled from an exponential distribution.
+
+        Parameters:
+            tau (float, optional): The mean (scale parameter) of the exponential distribution.
+                Defaults to 1.0.
+
+        Returns:
+            float: A random dwell time value sampled from the exponential distribution with mean `tau`.
+        """
+        return np.random.exponential(tau)
+
+    @staticmethod
+    def pretty_print(L):
+        r = ''
+        for el in L:
+            r += str(el)[1:-1].replace(',','') + '\n'
+        return r
+    
+    def __repr__(self):
+#         r = f"open states {self.states['open']}\n"
+#         r += f"close states {self.states['close']}\n"
+#         r += f"scheme\n{__class__.pretty_print(self.states['scheme'])}"
+        r = str(self.states)
+        return r
+    
+    def scheme_index(self, state, tau_hat):
+        border = len(self.states['open'])
+        if state == 'open':
+            idx = self.states['open'].index(tau_hat)
+        else:
+            idx = border + self.states['close'].index(tau_hat)
+        return idx
+    
+    def verboten_transition(self, prev_state, prev_tau_hat, new_state, new_tau_hat):
+        border = len(self.states['open'])
+        prev_idx = self.scheme_index(prev_state, prev_tau_hat)            
+        new_idx = self.scheme_index(new_state, new_tau_hat)
+        rate = self.states['scheme'][new_idx][prev_idx]
+        # print(prev_state, prev_tau_hat, new_state, new_tau_hat, rate)
+        return not bool(rate)
+    
+    def next_state(self, prev_state=None, prev_tau_hat=None):
+        # TODO: use rates provided in scheme
+        if prev_state == None:
+            new_state = np.random.choice(__class__.state)
+            new_tau_hat = np.random.choice(self.states[new_state])
+            return new_state, new_tau_hat
+        
+        new_state, new_tau_hat = prev_state, prev_tau_hat
+        while self.verboten_transition(prev_state, prev_tau_hat, new_state, new_tau_hat):
+            #print('Verboten!', end='___')
+            new_state = np.random.choice(__class__.state)
+            new_tau_hat = np.random.choice(self.states[new_state])
+        
+        return new_state, new_tau_hat
+    
+    #
+    # sections below are for defining forces and potentials
+    #
+    def vq(self, x, params={'a': 0, 'd': 0}):
+        a = params['a'] if 'a' in params else 0
+        d = params['d'] if 'd' in params else 0
+        return (x-a)**2 - d  # simple quadratic
+    
+    def fq(self, x, params={'a': 0}):
+        a = params['a'] if 'a' in params else 0
+        return -2*(x-a) # simple quadratic
+
+    def vm(self, x, params={'a': 0, 'b': 1, 'd': 0}):
+        a = params['a'] if 'a' in params else 0
+        b = params['b'] if 'b' in params else 1
+        d = params['d'] if 'd' in params else 0
+        return np.where(x < a, 
+                        self.vq(x, {'a': a, 'd': 0}), 
+                        b*self.vq(x, {'a': a, 'd': 0})) - d # modified quadratic on x<0
+
+    def fm(self, x, params={'a': 0, 'b': 1}):
+        a = params['a'] if 'a' in params else 0
+        b = params['b'] if 'b' in params else 1
+        return np.where(x < a, 
+                        self.fq(x, {'a': a}), 
+                        b*self.fq(x, {'a': a}))  # modified quadratic on x<0
+
+    def vp(self, x, params={'a': 0, 'b': 1, 'd': 0}):
+        a = params['a'] if 'a' in params else 0
+        b = params['b'] if 'b' in params else 1
+        d = params['d'] if 'd' in params else 0
+        return np.where(x > a, 
+                        self.vq(x, {'a': a, 'd': 0}), 
+                        b*self.vq(x, {'a': a, 'd': 0})) - d # modified quadratic on x>0
+
+    def fp(self, x, params={'a': 0, 'b': 1}):
+        a = params['a'] if 'a' in params else 0
+        b = params['b'] if 'b' in params else 1
+        return np.where(x > a, 
+                        self.fq(x, {'a': a}), 
+                        b*self.fq(x, {'a': a}))  # modified quadratic on x>0
+
+    def vb(self, x, params={'a': 0, 'b': 1, 'c': 1, 'd': 0}):
+        a = params['a'] if 'a' in params else 0
+        b = params['b'] if 'b' in params else 1
+        c = params['c'] if 'c' in params else 1
+        d = params['d'] if 'd' in params else 0
+        return b * (x-a) ** 4 / 4 - c * (x-a) ** 2 / 2 + d*x  # double well
+
+    def potential_asym_quadratic(self, x, 
+                                 params={'a': 1, 'bplus': 1, 'dplus': 0, 'bminus': 1, 'dminus': 0}):
+        a = params['a'] if 'a' in params else 1
+        bplus = params['bplus'] if 'bplus' in params else 1
+        dplus = params['dplus'] if 'dplus' in params else 0
+        bminus = params['bminus'] if 'bminus' in params else 1
+        dminus = params['dminus'] if 'dminus' in params else 0
+        return np.where(x > 0,
+                        self.vp(x, {'a': a, 'b': bplus, 'd': dplus}),
+                        self.vm(x, {'a': a, 'b': bminus, 'd': dminus}))
+
+    def force_asym_quadratic(self, x, 
+                             params={'a': 1, 'bplus': 1, 'bminus': 1}):
+        a = params['a'] if 'a' in params else 1
+        bplus = params['bplus'] if 'bplus' in params else 1
+        bminus = params['bminus'] if 'bminus' in params else 1
+        return np.where(x > 0, 
+                        self.fp(x, {'a': a, 'b': bplus}), 
+                        self.fm(x, {'a': a, 'b': bminus}))
+
+    def force_bistable(self, x, params={'a': 1, 'b': 1}):
+        '''double well
+        V = a / 4 * x**4 - b / 2 * x**2'''
+        a = params['a'] if 'a' in params else 1
+        b = params['b'] if 'b' in params else 1
+        return - a * x**3 + b * x
+
+    def potential_bistable(self, x, params={'a': 1, 'b': 1}):
+        a = params['a'] if 'a' in params else 1
+        b = params['b'] if 'b' in params else 1
+        return a / 4 * x**4 - b / 2 * x**2
+
+    def force_x2(self, x, params={'a': 1, 'b': 1}):
+        '''monostable at x=a
+        V = b / 2 * (x - a)**2'''
+        a = params['a'] if 'a' in params else 1
+        b = params['b'] if 'b' in params else 1
+        return -b * (x - a)
+
+    def potential_x2(self, x, params={'a': 1, 'b': 1}):
+        a = params['a'] if 'a' in params else 1
+        b = params['b'] if 'b' in params else 1
+        return b / 2 * (x - a)**2
+
+    def force_const(self, x, params={'a': 1}):
+        a = params['a'] if 'a' in params else 1
+        return -a
+
+    def potential_const(self, x, params={'a': 1}):
+        a = params['a'] if 'a' in params else 1
+        return a * x
+
+    def force(self, x, params={'a': 1, 'b': 1}):
+        '''potential'''
+        return self.force_x2(x, params)
+
+    def noise(self, rng, params={'a': 1}, levy_stat=True):
+        a = params['a'] if 'a' in params else 1
+        if levy_stat:
+            return my_levy_stable(1.25, 0, loc=a)
+        return rng.standard_normal()
+    
+    def simulate(self, tend=1, h=0.01, D=0.01, levy_stat=False, force=None):
+        rng = np.random.default_rng()
+        
+        if force == None:
+            raise ValueError('No force provided')
+        
+        if 'potential' not in force:
+            raise ValueError('No force type provided')
+        
+        if 'params' not in force:
+            raise ValueError('No force params provided')
+        force_params = force['params']
+
+        if force['potential'] == 'x2':
+            F = self.force_x2
+
+        elif force['potential'] == 'bistable':
+            raise NotImplementedError('Bistable dynamics not implemented')
+            F = self.force_bistable
+
+        elif force['potential'] == 'const':
+            F = self.force_const
+
+        elif force['potential'] == 'asym_quadratic':
+            F = self.force_asym_quadratic
+        
+        state, tau_hat = self.states['initial state']
+        tau = __class__.draw_dwell_time(tau_hat)
+
+        if self.full_stats:
+            self.update_stats((state, tau_hat, tau))
+
+        iks = self.__states[f'{state} value']
+        x = [iks]
+        t = 0
+        while t <= tend:
+            if t > tau:
+                # print('waaaat?', end='...')
+                state, tau_hat = self.next_state(state, tau_hat)
+
+                if tau_hat is None:
+                    raise ValueError("tau_hat cannot be None when drawing dwell time")
+                next_tau = __class__.draw_dwell_time(tau_hat)
+                
+                if self.full_stats: 
+                    self.update_stats((state, tau_hat, next_tau))
+                tau += next_tau
+                
+                if F.__name__ == self.force_const.__name__:
+                    iks = F(0, {'a': self.__states[f'{state} value']})
+                    
+                
+            else:
+                force_params['a'] = self.__states[f'{state} value']
+                if F.__name__ == self.force_const.__name__:
+                    iks = F(0, force_params)
+                else:
+                    iks += h * F(iks, force_params)
+                iks += (2 * D * h) ** 0.5 * self.noise(rng, {'a': self.__states[f'{state} value']}, 
+                                                       levy_stat=levy_stat)
+                           
+                x.append(iks)
+            t += h
+
+        return x
